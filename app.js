d5080dc125b9d5f3c0d74679545da61b543959ad
overlbjn <402309558@qq.com>
2016-10-09 15:53:59 +0800

    2
    2

--- app.js
+++ app.js
@@ -59,6 +59,17 @@ app.get('/test', function(req, res) {
   });  
 });
 
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === '9S98F9G9SFS65F4SDFGCVB') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
 app.post('/webhook', function (req, res) {
   var data = req.body;
 
